36d8d4d33a1f606cb0ba3b8e1129189ca908b187
Haggerty <dhaggerty@pronet.com>
2020-01-14 14:56:16 -0600

    functions

---
+++ chapter_1/funcs.js
@@ -0,0 +1,14 @@
+// function declaration
+function greet() {
+    console.log('hello there');
+}
+
+// function expression
+// expressions get semi colons at end of function
+const speak = function(){
+    console.log('good day!')
+};
+
+// function variable
+greet();
+speak();
