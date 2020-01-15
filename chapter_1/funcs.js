3da903033261d2af44f45be22933a5dd81d2c599
Haggerty <dhaggerty@pronet.com>
2020-01-15 08:32:13 -0600

    funcs done

--- chapter_1/funcs.js
+++ chapter_1/funcs.js
@@ -1,14 +1,35 @@
+
 // function declaration
-function greet() {
-    console.log('hello there');
+// declarations ARE hoisted
+// Thus declarations can be used prior to being written
+function employee() {
+    console.log('string employee');
 }
 
-// function expression
+// function expression (store a function inside a variable)
 // expressions get semi colons at end of function
-const speak = function(){
-    console.log('good day!')
-};
+const func_expression = function(){
+    console.log('Function expression & semi-colon');
+}; // semicolon here
 
-// function variable
-greet();
-speak();
+
+// returning a value stores in memory. Assign it to a variable when calling function
+// Passing a function as an argument to another JS function.
+// The argument function we pass in is called a callback function
+
+// can define callback function in the parameter
+let engineers = ['dhaggerty', 'jwatson', 'jdill'];
+engineers.forEach((employee, index) => console.log(`${index}, ${employee}`));
+
+// Or can define function as a variable and pass in as a callback function
+const logPerson = (employee, index) => console.log(`${index}, ${employee}`)
+engineers.forEach(logPerson);
+
+// get a reference to ul in index.html - get enineers html class .engineers
+const ul = document.querySelector('.engineers');
+
+let html = ``;
+
+engineers.forEach((person) => html += `<li style="color: purple">${person}</li>`);
+
+ul.innerHTML = html;
